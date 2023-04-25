e36491cf9a51a79f5cd9c5ee2e9e627d0e1e0391
Juan Diego Salazar <juadi19@gmail.com>
2023-04-25 17:49:20 -0600

    Agregando Sign Up

--- public/index.js
+++ public/index.js
@@ -1,12 +1,21 @@
-function autocomplete(inp, data) {
+function autocomplete(inp) {
   // the function take two arguments, the input data field and the data
-  console.log(data.nombre);
   var currentFocus;
-  inp.addEventListener("input", function (e) {
+  inp.addEventListener("input", async function (e) {
     var a,
       b,
       i,
+      movies,
       val = this.value;
+    const response = await fetch("/peliculas?s=" + val, {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    });
+    if (response.ok) {
+      movies = await response.json();
+      console.log(movies);
+    }
+
     closeAllLists(); // close any already open lists of autocompleted values
     if (!val) {
       return false;
@@ -16,54 +25,49 @@ function autocomplete(inp, data) {
     a.setAttribute("id", this.id + "autocomplete-list");
     a.setAttribute("class", "autocomplete-items");
     this.parentNode.appendChild(a); // append the div as a child of the autocomplete container
-    for (i = 0; i < data.length; i++) {
-      if (
-        // check if the item include the same letters as the text field value
-        data[i].nombre.toUpperCase().includes(val.toUpperCase())
-      ) {
-        b = document.createElement("DIV"); // div element for each matching element
-        // make the matching letters bold
-        if (data[i].nombre.toUpperCase().indexOf(val.toUpperCase()) > -1) {
-          // create a div element for each matching element
-          b = document.createElement("DIV");
-          // bold the matching substrings
-          let name = data[i].nombre;
-          let index = name.toUpperCase().indexOf(val.toUpperCase());
-          let matchedSubstring = name.substr(index, val.length);
-          let beforeSubstring = name.substr(0, index);
-          let afterSubstring = name.substr(index + val.length);
-          // concatenate the substrings with the bold tag
-          b.innerHTML =
-            beforeSubstring +
-            "<strong>" +
-            matchedSubstring +
-            "</strong>" +
-            afterSubstring;
-          // insert an input field that will hold the current data item's value:
-          b.innerHTML += "<input type='hidden' value='" + data[i].nombre + "'>";
-          // execute a function when someone clicks on the item value (DIV element):
-          b.addEventListener("click", function (e) {
-            // insert the value for the autocomplete text field:
-            inp.value = this.getElementsByTagName("input")[0].value;
-            // close the list of autocompleted values, (or any other open lists of autocompleted values):
-            closeAllLists();
-          });
-          // append the DIV element to the autocomplete container:
-          a.appendChild(b);
-        }
-
-        /*insert a input field that will hold the current dataay item's value:*/
-        b.innerHTML += "<input type='hidden' value='" + data[i].nombre + "'>";
-        /*execute a function when someone clicks on the item value (DIV element):*/
+    for (i = 0; i < movies.length; i++) {
+      b = document.createElement("DIV"); // div element for each matching element
+      // make the matching letters bold
+      if (movies[i].titulo.toUpperCase().indexOf(val.toUpperCase()) > -1) {
+        // create a div element for each matching element
+        b = document.createElement("DIV");
+        // bold the matching substrings
+        let name = movies[i].titulo;
+        let index = name.toUpperCase().indexOf(val.toUpperCase());
+        let matchedSubstring = name.substr(index, val.length);
+        let beforeSubstring = name.substr(0, index);
+        let afterSubstring = name.substr(index + val.length);
+        // concatenate the substrings with the bold tag
+        b.innerHTML =
+          beforeSubstring +
+          "<strong>" +
+          matchedSubstring +
+          "</strong>" +
+          afterSubstring;
+        // insert an input field that will hold the current data item's value:
+        b.innerHTML += "<input type='hidden' value='" + movies[i].titulo + "'>";
+        // execute a function when someone clicks on the item value (DIV element):
         b.addEventListener("click", function (e) {
-          /*insert the value for the autocomplete text field:*/
+          // insert the value for the autocomplete text field:
           inp.value = this.getElementsByTagName("input")[0].value;
-          /*close the list of autocompleted values,
-										(or any other open lists of autocompleted values:*/
+          // close the list of autocompleted values, (or any other open lists of autocompleted values):
           closeAllLists();
         });
+        // append the DIV element to the autocomplete container:
         a.appendChild(b);
       }
+
+      /*insert a input field that will hold the current dataay item's value:*/
+      b.innerHTML += "<input type='hidden' value='" + movies[i].titulo + "'>";
+      /*execute a function when someone clicks on the item value (DIV element):*/
+      b.addEventListener("click", function (e) {
+        /*insert the value for the autocomplete text field:*/
+        inp.value = this.getElementsByTagName("input")[0].value;
+        /*close the list of autocompleted values,
+										(or any other open lists of autocompleted values:*/
+        closeAllLists();
+      });
+      a.appendChild(b);
     }
   });
   /*execute a function presses a key on the keyboard:*/
@@ -124,17 +128,4 @@ function autocomplete(inp, data) {
   });
 }
 
-async function loadMovies() {
-  const response = await fetch("/peliculas", {
-    method: "GET",
-    headers: { "Content-Type": "application/json" },
-  });
-  if (response.ok) {
-    const movies = await response.json();
-    console.log(movies);
-
-    autocomplete(document.getElementById("myInput"), movies);
-  }
-}
-
-loadMovies();
+autocomplete(document.getElementById("myInput"));
