d76fd17e6c465990ec36cccefe12da6477422c49
Juan Diego Salazar <juadi19@gmail.com>
2023-04-25 22:53:39 -0600

    Login, Sign Up creados

--- public/index.js
+++ public/index.js
@@ -129,3 +129,52 @@ function autocomplete(inp) {
 }
 
 autocomplete(document.getElementById("myInput"));
+
+// Checking for a valid user
+
+function logOut() {
+  localStorage.removeItem("token");
+  window.location = "/login.html";
+}
+
+if (localStorage.getItem("token")) {
+  document.querySelector(".login").innerHTML = `
+  <div style="font-weight: bold;" id="loginText">Log Out</div>
+  <div class="icon-container">
+	  <a onclick="logOut()"class="icon" style="cursor: pointer"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+			  <path d="M217.9 105.9L340.7 228.7c7.2 7.2 11.3 17.1 11.3 27.3s-4.1 20.1-11.3 27.3L217.9 406.1c-6.4 6.4-15 9.9-24 9.9c-18.7 0-33.9-15.2-33.9-33.9l0-62.1L32 320c-17.7 0-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32l128 0 0-62.1c0-18.7 15.2-33.9 33.9-33.9c9 0 17.6 3.6 24 9.9zM352 416l64 0c17.7 0 32-14.3 32-32l0-256c0-17.7-14.3-32-32-32l-64 0c-17.7 0-32-14.3-32-32s14.3-32 32-32l64 0c53 0 96 43 96 96l0 256c0 53-43 96-96 96l-64 0c-17.7 0-32-14.3-32-32s14.3-32 32-32z"></path>
+		  </svg></a>
+  </div>`;
+} else {
+  document.querySelector(
+    ".login"
+  ).innerHTML = ` <div style="font-weight: bold;" id="loginText">Log In</div>
+  <div class="icon-container">
+	  <a href="/login.html" class="icon"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+			  <path d="M217.9 105.9L340.7 228.7c7.2 7.2 11.3 17.1 11.3 27.3s-4.1 20.1-11.3 27.3L217.9 406.1c-6.4 6.4-15 9.9-24 9.9c-18.7 0-33.9-15.2-33.9-33.9l0-62.1L32 320c-17.7 0-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32l128 0 0-62.1c0-18.7 15.2-33.9 33.9-33.9c9 0 17.6 3.6 24 9.9zM352 416l64 0c17.7 0 32-14.3 32-32l0-256c0-17.7-14.3-32-32-32l-64 0c-17.7 0-32-14.3-32-32s14.3-32 32-32l64 0c53 0 96 43 96 96l0 256c0 53-43 96-96 96l-64 0c-17.7 0-32-14.3-32-32s14.3-32 32-32z"></path>
+		  </svg></a>
+  </div>`;
+}
+
+document
+  .getElementById("selectedMovie")
+  .addEventListener("submit", async (event) => {
+    event.preventDefault();
+    console.log("aqui");
+    try {
+      const movieName = document.getElementById("myInput").value;
+      const response = await fetch("/peliculas/" + movieName, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (response.ok) {
+        const infoMovie = await response.json();
+        document.getElementById("infoMovie").innerHTML = infoMovie.autor;
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  });
